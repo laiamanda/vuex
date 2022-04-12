6892638ef940badfb102ae2bfed70090d1bc43ac
laiamanda <40805270+laiamanda@users.noreply.github.com>
2022-04-12 19:12:17 -0400

    Add ChangeCounter.vue

---
+++ src/components/ChangeCounter.vue
@@ -0,0 +1,30 @@
+<template>
+    <!-- <button @click="addOne">Add 1</button>
+         <button @click="inc">Add 1</button>
+     -->
+    <button @click="increment">Add 1</button>
+    <button @click="increase({value:10})">Add 1</button>
+</template>
+
+<script>
+
+import {mapActions} from 'vuex';
+
+export default{
+    methods:{
+        /* addOne(){
+            //this.$store.commit('increment');
+            this.$store.dispatch('increment');
+        } 
+        //...mapActions(['increment','increase'])
+         ...mapActions({
+            inc: 'increment',
+            increase: 'increase'
+        }) */
+         ...mapActions('numbers', {
+            inc: 'increment',
+            increase: 'increase'
+        })
+    }
+}
+</script>
